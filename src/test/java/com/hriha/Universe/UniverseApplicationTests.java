83ae6073c3cefa74ad6a257f9ae157f885d5d335
Mykhailo Hriha <mykhailho.hriha@gmail.com>
2020-12-11 19:56:56 +0200

    first commit

---
+++ src/test/java/com/hriha/Universe/UniverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hriha.Universe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
